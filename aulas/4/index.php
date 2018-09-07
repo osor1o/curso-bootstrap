e3dd13f90fbd279a88f1172e088e9e7191080b61
osor1o-note <osorioneto97@gmail.com>
2018-09-06 23:14:07 -0300

    Iniciando o repositório a partir da aula 8

---
+++ aulas/4/index.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Instalação bootstrap 4.1.x</title>
+        <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1>Olá Mundo!</h1>
+        
+        
+        <script src="../../js/jquery-3.3.1.slim.min.js"></script>
+        <script src="../../js/popper.min.js"></script>
+        <script src="../../js/bootstrap.min.js"></script> 
+    </body>
+</html>
+
---
+++ aulas/6/index.php
@@ -0,0 +1,141 @@
+<!Doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Instalação bootstrap 4.1.x</title>
+        <link rel="stylesheet" href="../../css/bootstrap.min.css">
+        <style>
+            div.bg{
+                background-color: #f00;
+                height: 50px;
+                border: 1px solid #fff;
+            }
+            div.bg1{
+                background-color: #00f;
+                height: 50px;
+                border: 1px solid #fff;
+                color: #fff;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+            </div>
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1 bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+                <div class="col bg">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1 bg">
+                    
+                </div>
+                <div class="col-6 bg">
+                    
+                </div>
+                <div class="col-2 bg">
+                    
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-1 bg">
+                    
+                </div>
+                <div class="col-6 bg">
+                    
+                </div>
+                <div class="col-2 bg">
+                    
+                </div>
+            </div>
+            <div class="row justify-content-end">
+                <div class="col-1 bg1">
+                    
+                </div>
+                <div class="col-6 bg1">
+                    
+                </div>
+                <div class="col-2 bg1">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-sm-12 bg1">
+                    
+                </div>
+                <div class="col-xl-4 col-sm-12 bg1">
+                    
+                </div>
+                <div class="col-xl-4 col-sm-12 bg1">
+                    
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-xl-4 bg1 order-2">
+                    <h1>primeiro item</h1>
+                </div>
+                <div class="col bg1 order-12">
+                    <h1>segundo item</h1>
+                </div>
+                <div class="col bg1 order-1">
+                    <h1>terceiro item</h1>
+                </div>
+            </div>
+        </div>
+        
+        <script src="../../js/jquery-3.3.1.slim.min.js"></script>
+        <script src="../../js/popper.min.js"></script>
+        <script src="../../js/bootstrap.min.js"></script> 
+    </body>
+</html>
+
